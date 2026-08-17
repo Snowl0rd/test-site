b468acbe9bd681ca9f19bc2c5405c0a6c14364b5
Snowl0rd <122724051+Snowl0rd@users.noreply.github.com>
2026-08-17 22:04:11 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,3 +15,32 @@ function nextSlide() {
 }
 
 setInterval(nextSlide, 7000);
+
+//МЕНЮ
+const menuButton = document.querySelector(".menu-toggle");
+const navigation = document.querySelector(".nav-menu");
+
+menuButton.addEventListener("click", function () {
+
+    navigation.classList.toggle("active");
+
+    if (navigation.classList.contains("active")) {
+        menuButton.querySelector("span").textContent = "×";
+    } else {
+        menuButton.querySelector("span").textContent = "☰";
+    }
+
+});
+
+const menuLinks = document.querySelectorAll(".nav-menu a");
+
+menuLinks.forEach(function (link) {
+
+    link.addEventListener("click", function () {
+
+        navigation.classList.remove("active");
+        menuButton.querySelector("span").textContent = "☰";
+
+    });
+
+});
